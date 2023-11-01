cc045610949df5f07c1f58ed1a460314903ae07c
RyanSmith57 <80649729+RyanSmith57@users.noreply.github.com>
2023-11-01 19:20:05 +1300

    added player and enemy attack boxes

--- index.js
+++ index.js
@@ -8,16 +8,27 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 class Sprite {
-    constructor({position, velocity}) {
+    constructor({position, velocity, color = 'red'}) {
         this.position = position
         this.velocity = velocity
+        this.width = 50
         this.height = 150
         this.lastkey
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+        }
+        this.color = color
     }
 
 draw() {
-    c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, 50, this.height)
+    c.fillStyle = this.color
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+
+    //attack box
+    c.fillStyle = 'grey'
+    c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
 }
 
 update() {          
@@ -53,7 +64,8 @@ const enemy = new Sprite({
     velocity: {
         x: 0,
         y: 0
-    }
+    },
+    color: 'blue'
 })
 
 
@@ -100,6 +112,15 @@ function animate() {
         enemy.velocity.x = -3
     } else if (keys.ArrowRight.pressed && enemy.lastkey === 'ArrowRight'){
         enemy.velocity.x = 3
+    }
+
+    //collision detection
+    if(player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
+         player.attackBox.position.x <= enemy.position.x + enemy.width &&
+          player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+          && player.attackBox.position.y <= enemy.position.y + enemy.height
+          ){
+        console.log('ew');
     }
 }
 
